64ae39c1ad5ad31d3ec53be6d88b50a57206b691
Tanya L. Crenshaw <tcrenshaw@newrelic.com>
2018-01-11 04:39:15 +0000

    Fix tabbing.

--- examples/ex_notice_error.c
+++ examples/ex_notice_error.c
@@ -8,28 +8,29 @@
 #include "ex_common.h"
 #include "libnewrelic.h"
 
-void record_error(newrelic_txn_t* txn){
+void record_error(newrelic_txn_t* txn) {
   int priority = 50;
   newrelic_notice_error(txn, priority, "Meaningful error message",
                         "Error.class.supervalu");
   return;
 }
 
-
 int main(void) {
   newrelic_app_t* app = 0;
   newrelic_txn_t* txn = 0;
   newrelic_config_t* config = 0;
 
-	char * app_name = get_app_name();
-	if (app_name == NULL) return -1;
+  char* app_name = get_app_name();
+  if (app_name == NULL)
+    return -1;
 
-	char * license_key = get_license_key();
-	if (license_key == NULL) return -1;
+  char* license_key = get_license_key();
+  if (license_key == NULL)
+    return -1;
 
   config = newrelic_new_config(app_name, license_key);
 
-	customize_config(&config);
+  customize_config(&config);
 
   /* Wait up to 10 seconds for the agent to connect to the daemon */
   app = newrelic_create_app(config, 10000);
