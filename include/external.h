3cf18475bfd3debdadfaf1b14df93d183ec0e115
Adam Harvey <aharvey@php.net>
2018-01-23 19:08:18 -0800

    Improve documentation.

--- include/external.h
+++ include/external.h
@@ -4,11 +4,27 @@
 #include "node_external.h"
 #include "nr_txn.h"
  
+/*! @brief The internal type used to represent an external segment. */
 typedef struct _newrelic_external_segment_t {
+  /*! The transaction the external segment was created on. */
   nrtxn_t *txn;
+
+  /*! The external node parameters. */
   nr_node_external_params_t params;
 } newrelic_external_segment_t;
 
+/*!
+ * @brief Validate an external parameter.
+ *
+ * This function ensures that any given parameter does not include a slash. As
+ * external parameter values are generally used in metric names, slashes will
+ * break the APM UI.
+ *
+ * @param [in] in   The parameter value.
+ * @param [in] name The parameter name (used in any error message).
+ * @return True if the parameter value is valid, false otherwise. A message at
+ *         level LOG_ERROR will be logged if validation fails.
+ */
 bool newrelic_validate_external_param(const char* in, const char* name);
 
 #endif /* LIBNEWRELIC_EXTERNAL_H */
--- src/external.c
+++ src/external.c
@@ -21,6 +21,7 @@ newrelic_external_segment_t* newrelic_start_external_segment(newrelic_txn_t* tra
                                                              const newrelic_external_segment_params_t *params) {
   newrelic_external_segment_t* segment = NULL;
 
+  /* Validate our inputs. */
   if (NULL == transaction) {
     nrl_error(NRL_INSTRUMENT, "cannot start an external segment on a NULL transaction");
     return NULL;
@@ -44,7 +45,11 @@ newrelic_external_segment_t* newrelic_start_external_segment(newrelic_txn_t* tra
     return NULL;
   }
 
+  /* We zero-allocate to ensure that the nr_node_external_params_t embedded
+   * within the newrelic_external_segment_t struct is correctly initialised. */
   segment = nr_zalloc(sizeof(newrelic_external_segment_t));
+
+  /* Set the fields that we care about. */
   nr_txn_set_time(transaction, &segment->params.start);
   segment->txn              = transaction;
   segment->params.library   = params->library   ? nr_strdup(params->library)   : NULL;
@@ -56,31 +61,44 @@ newrelic_external_segment_t* newrelic_start_external_segment(newrelic_txn_t* tra
 
 bool newrelic_end_external_segment(newrelic_txn_t* transaction,
                                    newrelic_external_segment_t** segment_ptr) {
+  bool status = false;
   newrelic_external_segment_t* segment = NULL;
 
-  if (NULL == transaction) {
-    nrl_error(NRL_INSTRUMENT, "cannot end an external segment on a NULL transaction");
-    return false;
-  }
-
+  /* Validate our inputs. We can only early return from the segment check
+   * because the documented behaviour is to destroy any segment that's passed
+   * in. */
   if ((NULL == segment_ptr) || (NULL == *segment_ptr)) {
     nrl_error(NRL_INSTRUMENT, "cannot end a NULL external segment");
     return false;
   }
 
+  if (NULL == transaction) {
+    nrl_error(NRL_INSTRUMENT, "cannot end an external segment on a NULL transaction");
+    goto end;
+  }
+
+  /* Sanity check that the external segment is being ended on the same
+   * transaction it was started on. Transitioning an external segment between
+   * transactions would be problematic, since times are transaction-specific.
+   * */
   segment = *segment_ptr;
   if (transaction != segment->txn) {
     nrl_error(NRL_INSTRUMENT, "cannot end an external segment on a different transaction to the one it was created on");
-    return false;
+    goto end;
   }
 
+  /* Stop the transaction and save the node. */
   nr_txn_set_time(transaction, &segment->params.stop);
   nr_txn_end_node_external(transaction, &segment->params);
 
+  status = true;
+
+end:
+  /* Destroy the external segment. */
   nr_free(segment->params.library);
   nr_free(segment->params.procedure);
   nr_free(segment->params.url);
   nr_realfree((void **) segment_ptr);
 
-  return true;
+  return status;
 }
