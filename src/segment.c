4ac8baca9661d5a4253e9c9f2bdc721dab301692
Tanya L. Crenshaw <tcrenshaw@newrelic.com>
2018-01-26 21:32:03 +0000

    Polish formatting with clang-format.

--- src/segment.c
+++ src/segment.c
@@ -4,16 +4,15 @@
 #include "util_strings.h"
 
 bool newrelic_validate_segment_param(const char* in, const char* name) {
-
   if (nr_strchr(in, '/')) {
-  	if(NULL == name) {
-			nrl_error(NRL_INSTRUMENT, "parameter cannot include a slash");
-		}
-	  else {
-	  	nrl_error(NRL_INSTRUMENT, "%s cannot include a slash", name);
-	  }
-  	return false;
+    if (NULL == name) {
+      nrl_error(NRL_INSTRUMENT, "parameter cannot include a slash");
+    } else {
+      nrl_error(NRL_INSTRUMENT, "%s cannot include a slash", name);
+    }
+    return false;
   }
 
   return true;
 }
+
