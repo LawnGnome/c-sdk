14d547e75e0c89596f6f0e872f55b16260204327
Adam Harvey <aharvey@php.net>
2018-01-30 09:28:52 -0800

    NULL first you must test.

--- examples/ex_external.c
+++ examples/ex_external.c
@@ -19,11 +19,11 @@ int main(void) {
   newrelic_external_segment_t* segment = 0;
 
   char* app_name = get_app_name();
-  if (app_name == NULL)
+  if (NULL == app_name)
     return -1;
 
   char* license_key = get_license_key();
-  if (license_key == NULL)
+  if (NULL == license_key)
     return -1;
 
   config = newrelic_new_config(app_name, license_key);
